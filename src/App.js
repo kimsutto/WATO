31d4ba32cde2c3b34ff0ca12ea828a6bf9186144
kimsutto <themerciless2016@gmail.com>
2020-11-27 20:56:00 +0900

    불필요한 파일 삭제

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+const title = 'Wato';
+const cop = {
+  name : 'HIFI',
+  member : 'MJ, SJ, EH',
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+      <h1>Hello {title}</h1>
+      <h2>{cop.name} {cop.member} </h2>
+</div> );
 }
 
 export default App;
