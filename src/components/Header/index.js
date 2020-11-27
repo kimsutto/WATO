bd90ef0e45f5234e485a6277e451ac43ee6f49eb
kimsutto <themerciless2016@gmail.com>
2020-11-28 02:29:47 +0900

    공통 헤더 추가

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {Route, Link} from 'react-router-dom';
+import {Container,Name,MenuWrapper,Menu} from './style.js';
 
 const Header = () =>{
     return (
-        <>
-            <p>Header</p>
-        </>
+        <Container>
+            <Name><Link to ='/'>WATO</Link></Name>
+            <MenuWrapper>
+                <Menu><Link to ='/'>Home</Link></Menu>
+                <Menu><Link to ='/party'>Party</Link></Menu>
+                <Menu><Link to ='/mypage'>MyPage</Link></Menu>
+            </MenuWrapper>
+        </Container>
     );
 };
 
---
+++ src/components/Header/style.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+    display: block;
+    top: 0px
+    position: fixed;
+    background-color:white;
+`;
+export const Name = styled.div `
+     display: inline-block;
+     padding: 5px 10px;
+     margin: 10px 10px;
+ `;
+
+export const MenuWrapper = styled.div `
+     display: inline-block;
+     float: right;
+     margin: 5px 0px 5px 10px; 
+ `;
+export const Menu = styled.span `
+     display: inline-block;
+     padding: 5px;
+     margin: 5px 15px;
+ `;
