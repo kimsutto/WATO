706798fe2e21a06061c13831f7bca4828b70a9f4
kimsutto <themerciless2016@gmail.com>
2020-12-01 16:45:00 +0900

    Home 컴포넌트 수정

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import {React,useEffect,useState} from 'react';
 import styled from 'styled-components';
 import {Route, Link} from 'react-router-dom';
-import Header from '../../components/Header'
+import Header from '../../components/Header';
+import logo from '../../images/logo.jpg';
 
 const Container = styled.div`
     margin-top: 50px;
     display: flex;
     flex-direction: column;
     align-items: center;
-    font-size: 15px;
+    font-size: 17px;
     color: #5e5e5e;
 `;
 
@@ -25,25 +26,40 @@ const Button = styled.button`
 `;
 
 const Title = styled.div`
-    font-size: 25px;
+    width : 270px;
     font-weight: bold;
     margin-bottom : 50px;
 `;
 
 const Home = () => {
+    
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    useEffect(
+        () =>{
+            if(sessionStorage.getItem('walletInstance')){
+                setIsLoggedIn(true);
+            }
+        },[],
+    );
+    
     return(
         <>
             <Header/>
             <Container>
-                <Title>Watch Together!</Title>
+                <Title><img src = {logo} /></Title>
                 <p>보고싶은 영화, 드라마, 예능!</p>
                 <p>우리 같이 싸게 볼래?</p>
+                <br/>
                 <br/>
                 <p>WATO는 ott 플랫폼의 계정을 파티원들과</p>
                 <p>공유하여 요금을 나누어 낼 수 있는</p>
                 <p>파티를 매칭해줍니다!</p>
-                
-                <Button><Link to ='/login'>시작하기</Link></Button>
+
+                {isLoggedIn ? (
+                    <Button><Link to ='/selectplatform'>파티찾기</Link></Button>
+                ):(
+                    <Button><Link to ='/login'>시작하기</Link></Button>
+                )}
             </Container> 
         </>
     );
